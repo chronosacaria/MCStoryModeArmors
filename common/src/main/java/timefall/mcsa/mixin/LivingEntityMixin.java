8ee05fb86b41719ff4253f24c5615e0594b1d0e7
chronosacaria <chronos.droid@gmail.com>
2022-09-13 15:12:43 -0400

    Fixed a crash with LivingEntityMixin#mcsa$damageModifiers  - Fixed crash whereby a player would crash when being attacked

--- common/src/main/java/timefall/mcsa/mixin/LivingEntityMixin.java
+++ common/src/main/java/timefall/mcsa/mixin/LivingEntityMixin.java
@@ -33,50 +33,71 @@ public class LivingEntityMixin {
 
     @ModifyVariable(method = "damage", at = @At(value = "HEAD"), argsOnly = true)
     public float mcsa$damageModifiers(float amount, DamageSource source) {
-        LivingEntity livingEntity = (LivingEntity) (Object) this;
+        if (!(source.getAttacker() instanceof LivingEntity armoredEntity)) {
+            return amount;
+        }
 
-        if (CleanlinessHelper.hasArmorSet(livingEntity, ArmorsInit.ADAMANTIUM_ARMOR, ArmorSets.ADAMANTIUM)) {
-            if (livingEntity.getAttacker() != null && livingEntity.getAttacker().getMainHandStack().getItem() instanceof ToolItem || source.isProjectile())
+        LivingEntity target = (LivingEntity) (Object) this;
+
+        if (CleanlinessHelper.hasArmorSet(armoredEntity, ArmorsInit.ADAMANTIUM_ARMOR, ArmorSets.ADAMANTIUM)) {
+            if (armoredEntity.getMainHandStack() != null
+                    && armoredEntity.getMainHandStack().getItem() instanceof ToolItem || source.isProjectile())
                 return amount * 0.10f;
         }
 
-        if (CleanlinessHelper.hasArmorSet(livingEntity, ArmorsInit.DRAGONSBANE, ArmorSets.DRAGONSBANE)) {
-            if (livingEntity.getAttacking() instanceof EnderDragonEntity)
+        if (CleanlinessHelper.hasArmorSet(armoredEntity, ArmorsInit.DRAGONSBANE, ArmorSets.DRAGONSBANE)) {
+            if (target instanceof EnderDragonEntity)
                 return amount * 1.25f;
         }
 
-        if (CleanlinessHelper.hasArmorSet(livingEntity, ArmorsInit.ENDER_DEFENDER, ArmorSets.ENDER_DEFENDER)) {
-            if (livingEntity.getAttacking() instanceof EndermanEntity)
+        if (CleanlinessHelper.hasArmorSet(armoredEntity, ArmorsInit.ENDER_DEFENDER, ArmorSets.ENDER_DEFENDER)) {
+            if (target instanceof EndermanEntity)
                 return amount * 1.5f;
         }
 
-        if (CleanlinessHelper.hasArmorSet(livingEntity, ArmorsInit.GOLDEN_GOLIATH_ARMOR, ArmorSets.GOLDEN_GOLIATH)) {
-            if (livingEntity.getAttacking() != null && (livingEntity.getWorld().isDay() || livingEntity.getWorld().getMoonPhase() == 1)
-                    && livingEntity.getWorld().isSkyVisible(livingEntity.getBlockPos())) {
-                livingEntity.getAttacking().setOnFireFor(5);
+        if (CleanlinessHelper.hasArmorSet(armoredEntity, ArmorsInit.GOLDEN_GOLIATH_ARMOR, ArmorSets.GOLDEN_GOLIATH)) {
+            // If it is day or the moon is full
+            if (armoredEntity.getWorld().isDay() || armoredEntity.getWorld().getMoonPhase() == 1
+                    // If the player can see the sky (i.e. is not in a cave or underground)
+                && armoredEntity.getWorld().isSkyVisible(armoredEntity.getBlockPos())) {
+                target.setOnFireFor(5);
                 return amount * 2f;
             } else {
                 return amount * 0.5f;
             }
         }
 
-        if (CleanlinessHelper.hasArmorSet(livingEntity, ArmorsInit.MAGNUS_ARMOR, ArmorSets.MAGNUS)) {
+        if (CleanlinessHelper.hasArmorSet(armoredEntity, ArmorsInit.MAGNUS_ARMOR, ArmorSets.MAGNUS)) {
             if (source.isExplosive())
                 return amount / 2;
         }
 
-        if (CleanlinessHelper.hasArmorSet(livingEntity, ArmorsInit.REDSTONE_RIOT, ArmorSets.REDSTONE_RIOT)) {
-            if (livingEntity.getAttacker() != null && livingEntity.getAttacker().getMainHandStack().getItem() instanceof ToolItem || source.isProjectile())
+        if (CleanlinessHelper.hasArmorSet(armoredEntity, ArmorsInit.REDSTONE_RIOT, ArmorSets.REDSTONE_RIOT)) {
+            if (armoredEntity.getAttacker() != null
+                    && armoredEntity.getAttacker().getMainHandStack() != null
+                    && armoredEntity.getAttacker().getMainHandStack().getItem() instanceof ToolItem || source.isProjectile())
                 return amount * 0.25f;
         }
 
-        if (CleanlinessHelper.hasArmorSet(livingEntity, ArmorsInit.SWORDBREAKER, ArmorSets.SWORDBREAKER)) {
-            if (livingEntity.getAttacker() != null && livingEntity.getAttacker().getMainHandStack().getItem() instanceof SwordItem) {
-                livingEntity.getAttacker().getMainHandStack().damage(50, livingEntity.getAttacker(), (entity) -> livingEntity.getAttacker().sendEquipmentBreakStatus(EquipmentSlot.MAINHAND));
+        if (CleanlinessHelper.hasArmorSet(armoredEntity, ArmorsInit.STAR_SHIELD, ArmorSets.STAR_SHIELD)) {
+            if (armoredEntity.getAttacker() != null
+                    && armoredEntity.getAttacker().getMainHandStack() != null
+                    && (armoredEntity.getAttacker().getMainHandStack().isOf(Items.NETHERITE_AXE) || armoredEntity.getAttacker().getMainHandStack().isOf(Items.NETHERITE_SWORD))) {
                 return amount * 0;
             }
-            if (livingEntity.getAttacker() != null && livingEntity.getAttacker().getMainHandStack().getItem() instanceof AxeItem) {
-                livingEntity.getAttacker().getMainHandStack().damage(1, livingEntity.getAttacker(), (entity) -> livingEntity.getAttacker().sendEquipmentBreakStatus(EquipmentSlot.MAINHAND));
+        }
+
+        if (CleanlinessHelper.hasArmorSet(armoredEntity, ArmorsInit.SWORDBREAKER, ArmorSets.SWORDBREAKER)) {
+            if (armoredEntity.getAttacker() != null
+                    && armoredEntity.getAttacker().getMainHandStack() != null
+                    && armoredEntity.getAttacker().getMainHandStack().getItem() instanceof SwordItem) {
+                armoredEntity.getAttacker().getMainHandStack().damage(50, armoredEntity.getAttacker(), (entity) -> armoredEntity.getAttacker().sendEquipmentBreakStatus(EquipmentSlot.MAINHAND));
+                return amount * 0;
+            }
+            if (armoredEntity.getAttacker() != null
+                    && armoredEntity.getAttacker().getMainHandStack() != null
+                    && armoredEntity.getAttacker().getMainHandStack().getItem() instanceof AxeItem) {
+                armoredEntity.getAttacker().getMainHandStack().damage(1, armoredEntity.getAttacker(), (entity) -> armoredEntity.getAttacker().sendEquipmentBreakStatus(EquipmentSlot.MAINHAND));
                 return amount * 1.25f;
             }
         }
