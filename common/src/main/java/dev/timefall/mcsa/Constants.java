05c6def81e5dadfa23f14a7f3d4b2c68e2fac149
chronosacaria <chronos.droid@gmail.com>
2026-09-08 13:32:30 -0400

    Update MCSA to be Multiloader - Started setting up MCSA to be able to support multiloader development in anticipation for 1.21.x update process

---
+++ common/src/main/java/dev/timefall/mcsa/Constants.java
@@ -0,0 +1,11 @@
+package dev.timefall.mcsa;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "mcsa";
+	public static final String MOD_NAME = "MC Story Mode Armors";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
